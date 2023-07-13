26a28fc6a6b2eacb180811a0aade17455e4da933
piac <giulio@mcneel.com>
2023-07-13 18:24:02 +0200

    Add tests

--- SimpleUnitTests/TestCircle.cs
+++ SimpleUnitTests/TestCircle.cs
@@ -9,7 +9,31 @@ namespace SimpleeRHTests
     public class PrimitivesFixture : Rhino.Test.RhinoTestFixture
     {
         [Test]
-        public void TestCircle()
+        public void TestSDKCircle()
+        {
+            var circle = new Circle(12);
+            Assert.AreEqual(12, circle.Radius);
+            Assert.AreEqual(12.0 * Math.PI * 2, circle.Circumference);
+        }
+
+        [Test]
+        public void TestCircleInMemoryDocument()
+        {
+            var circle = new Circle(12);
+            Assert.AreEqual(12, circle.Radius);
+            Assert.AreEqual(12.0 * Math.PI * 2, circle.Circumference);
+        }
+
+        [Test]
+        public void TestCircleInDocument()
+        {
+            var circle = new Circle(12);
+            Assert.AreEqual(12, circle.Radius);
+            Assert.AreEqual(12.0 * Math.PI * 2, circle.Circumference);
+        }
+
+        [Test]
+        public void TestCircleInGrasshopper()
         {
             var circle = new Circle(12);
             Assert.AreEqual(12, circle.Radius);
