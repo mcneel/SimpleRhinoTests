a00b600dfdcd1294a10e098d5414e6e4766e4421
eirannejad <eirannejad@gmail.com>
2024-02-29 17:36:57 -0800

    added rhino doc tests

---
+++ SimpleUnitTests/TestDocument.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+using Rhino;
+using Rhino.Testing.Fixtures;
+
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace SimpleNUnitTests
+{
+    [TestFixture]
+    public sealed class TestDocument : RhinoTestFixture
+    {
+        [Test]
+        public void TestOpen()
+        {
+            string output = Path.GetDirectoryName(GetType().Assembly.Location);
+            string modelPath = Path.Combine(output, @"Files\circle.3dm");
+
+            Assert.DoesNotThrow(() => RhinoDoc.Open(modelPath, out bool _));
+        }
+
+        [Test]
+        public void TestOpenHeadless()
+        {
+            string output = Path.GetDirectoryName(GetType().Assembly.Location);
+            string modelPath = Path.Combine(output, @"Files\circle.3dm");
+
+            Assert.DoesNotThrow(() => RhinoDoc.OpenHeadless(modelPath));
+        }
+    }
+}
